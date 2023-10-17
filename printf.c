ddeaafd28d5de20838404b8747dde089576334ec
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2023-10-17 15:24:54 +0000

    Updated the null format

--- printf.c
+++ printf.c
@@ -4,10 +4,10 @@
 /**
  * _printf - This is a function used to print a formatted output
  *
- * @format: A character string
+ * @format: The format in which the argument will be printed
  * @...: Other arguments
  *
- * Return: 0 success
+ * Return: no. of characters
  */
 
 int _printf(const char *format, ...)
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 
 	int printed_chars = 0;
 
-	if (format == NULL)
+	if (*format == NULL)
 		return (1);
 
 	va_start(argument_lists, format);
